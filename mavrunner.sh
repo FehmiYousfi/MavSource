20126044fb037e6e685c81962715ff5d5bd8d4ac
Fehmi_Yousfi_VM_Luceor <fehmi.yousfi@etudiant-enit.utm.tn>
2024-10-25 14:18:58 +0100

    update struct

--- mavrunner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-LOG_DIR=/var/log/mavrouter
-STDOUT_LOG=$LOG_DIR/mavrouter.log
-
-[ ! -d $LOG_DIR ] && mkdir -p $LOG_DIR
-
-MAVRouterPorted -c /etc/config/mavconf.conf -e 192.168.1.1:11001 >> $STDOUT_LOG 2>&1
-
