efcb20b814684aca9a57c09029b6fe6a26c419d6
FehmiYousfi <118828796+FehmiYousfi@users.noreply.github.com>
2024-10-25 14:09:06 +0100

    Update mavrunner.sh

--- mavrunner.sh
+++ mavrunner.sh
@@ -5,5 +5,5 @@ STDOUT_LOG=$LOG_DIR/mavrouter.log
 
 [ ! -d $LOG_DIR ] && mkdir -p $LOG_DIR
 
-MAVRouterPorted -c /etc/config/MAVRouterPorted/mavconf.conf -e 192.168.1.1:11001 >> $STDOUT_LOG 2>&1
+MAVRouterPorted -c /etc/config/mavconf.conf -e 192.168.1.1:11001 >> $STDOUT_LOG 2>&1
 
